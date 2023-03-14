4f255716d88cb10c5a73295d66dc513dbab0ef03
Yun Jang <jang.yunj7@gmail.com>
2023-03-13 23:17:37 -0400

    same as using

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 
 // typedef std::vector<std::pair<std::string, int>> pairlist_t;
 
-typedef std::string text_t; // text_t is an identifier
-typedef int number_t;
+// typedef std::string text_t; // text_t is an identifier
+// typedef int number_t;
+
+//---SAME AS---
+using text_t = std::string;
+using number_t = int;
 
 int main()
 {
@@ -22,5 +26,3 @@ endl - flushes the buffer.
 
 To flush, means to empty the buffer. Now, buffer is temporary storage area for storing data. Both endl and \n can be used to print newline character but there is minor difference between these two : In case of endl, buffer is cleared by usage of internal call to flush the buffer.
 */
-
-#include <vector>
