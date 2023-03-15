d06c474ed514e76b9a1c4e2ac5694d9cbfb1bea5
Yun Jang <jang.yunj7@gmail.com>
2023-03-14 21:26:44 -0400

    type conversion

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-#include <vector>
-
-// typedef std::vector<std::pair<std::string, int>> pairlist_t;
-
-// typedef std::string text_t; // text_t is an identifier
-// typedef int number_t;
-
-//---SAME AS---
-using text_t = std::string;
-using number_t = int;
 
 int main()
 {
-    text_t firstName = "Bro";
-    number_t age = 21;
+    int correct = 8;
+    int questions = 10;
 
-    std::cout << firstName << '\n';
-    std::cout << age << '\n';
+    double score = correct / (double)questions * 100; // type conversion is used here so that division will return a double
+    std::cout << score << "%";
 }
-
-/*
-endl vs. \n
-endl - flushes the buffer.
-\n - new line
-
-To flush, means to empty the buffer. Now, buffer is temporary storage area for storing data. Both endl and \n can be used to print newline character but there is minor difference between these two : In case of endl, buffer is cleared by usage of internal call to flush the buffer.
-*/
