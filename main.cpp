b1fb85eff8936cd0e79bbe526b394a093bbd1e5a
Yun Jang <jang.yunj7@gmail.com>
2023-03-15 21:17:11 -0400

    switch case

--- main.cpp
+++ main.cpp
@@ -2,20 +2,51 @@
 
 int main()
 {
-    int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    // switch - alternative to else if statements; compares one value against matching cases
 
-    if (age >= 18)
+    int month;
+    std::cout << "Enter the month (1-12): ";
+    std::cin >> month;
+
+    switch (month)
     {
-        std::cout << "You are 18 or over";
-    }
-    else if (age < 0)
-    {
-        std::cout << "Invalid number";
-    }
-    else
-    {
-        std::cout << "You are under 18";
+    case 1:
+        std::cout << "January";
+        break;
+    case 2:
+        std::cout << "February";
+        break;
+    case 3:
+        std::cout << "March";
+        break;
+    case 4:
+        std::cout << "April";
+        break;
+    case 5:
+        std::cout << "May";
+        break;
+    case 6:
+        std::cout << "June";
+        break;
+    case 7:
+        std::cout << "July";
+        break;
+    case 8:
+        std::cout << "August";
+        break;
+    case 9:
+        std::cout << "September";
+        break;
+    case 10:
+        std::cout << "October";
+        break;
+    case 11:
+        std::cout << "November";
+        break;
+    case 12:
+        std::cout << "December";
+        break;
+    default:
+        std::cout << "Enter a valid number (1-12)";
     }
 }
