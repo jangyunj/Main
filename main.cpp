d1139a53756208c0006e3f39cd744dbdb7bb2ec3
Yun Jang <jang.yunj7@gmail.com>
2023-03-15 21:04:32 -0400

    if else

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <cmath>
 
-//--------Let's calculate the hypothenuse of a triangle--------
 int main()
 {
-    double a;
-    double b;
-    double c;
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
 
-    std::cout << "Enter length of side A: ";
-    std::cin >> a;
-
-    std::cout << "Enter length of side B: ";
-    std::cin >> b;
-
-    c = sqrt(pow(a, 2) + pow(b, 2));
-
-    std::cout << "Length of side C: " << c;
+    if (age >= 18)
+    {
+        std::cout << "You are 18 or over";
+    }
+    else if (age < 0)
+    {
+        std::cout << "Invalid number";
+    }
+    else
+    {
+        std::cout << "You are under 18";
+    }
 }
