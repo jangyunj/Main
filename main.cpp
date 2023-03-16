3e133b12dae13e35168ad78058bb9a70a16efcd0
Yun Jang <jang.yunj7@gmail.com>
2023-03-16 00:38:54 -0400

    temp conversion

--- main.cpp
+++ main.cpp
@@ -2,39 +2,36 @@
 
 int main()
 {
-    int temp;
-    bool sunny = false;
-    // std::cout << "Enter the temperature in Celcius: ";
-    // std::cin >> temp;
+    double temp;
+    char unit;
 
-    // if (temp > 0 && temp < 30){
-    //     std::cout << "The temperature is good";
-    // } else {
-    //     std::cout << "The temperature is bad";
-    // }
+    std::cout << "***Temperature Conversion*** \n";
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celcius\n";
+    std::cout << "What unit would you like to convert to? ";
 
-    // if (temp <= 0 || temp >= 30){
-    //     std::cout << "The temperature is good";
-    // } else {
-    //     std::cout << "The temperature is bad";
-    // }
+    std::cin >> unit;
 
-    if (sunny)
+    if (unit == 'F' || unit == 'f')
     {
-        std::cout << "It is sunny today";
+        std::cout << "Enter the temperature in Celcius: ";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Temperature is: " << temp << "F\n";
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
+        std::cout << "Enter the temperature in Fahrenheit: ";
+        std::cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        std::cout << "Temperature is: " << temp << "C\n";
     }
     else
     {
-        std::cout << "It is cloudy today";
+        std::cout << "Enter a valid temperature";
     }
 
-    //--OR--
-    if (!sunny)
-    {
-        std::cout << "It is cloudy today";
-    }
-    else
-    {
-        std::cout << "It is sunny today";
-    }
+    std::cout << "****************************";
 }
