545dccc904f1d19fae31619ca1280dd0f1da7bab
Yun Jang <jang.yunj7@gmail.com>
2023-03-17 17:06:08 -0400

    break, continue

--- main.cpp
+++ main.cpp
@@ -2,15 +2,23 @@
 
 int main()
 {
-    // For loops are used when codes are executed for CERTAIN NUMBER of times
-    for (int i = 10; i >= 0; i--)
+    // break : break out of a loop
+    // continue : skip current iteration
+
+    for (int i = 1; i <= 20; i++)
     {
+        if (i == 13)
+        {
+            break;
+        }
         std::cout << i << '\n';
     }
-
-    // decrement by 2
-    for (int i = 10; i >= 0; i -= 2)
+    for (int i = 1; i <= 20; i++)
     {
+        if (i == 13)
+        {
+            continue;
+        }
         std::cout << i << '\n';
     }
 }
