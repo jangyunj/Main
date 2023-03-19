45b0353aa86edf668b3ce3b2fedab9699cafd432
Yun Jang <jang.yunj7@gmail.com>
2023-03-19 14:01:04 -0400

    function

--- main.cpp
+++ main.cpp
@@ -1,33 +1,16 @@
+// function = a block of reusable code
 #include <iostream>
-#include <cmath>
 
+void happyBirthday()
+{
+    std::cout << "happy birthday to you\n";
+    std::cout << "happy birthday to you\n";
+    std::cout << "happy birthday dear you\n";
+    std::cout << "happy birthday to you\n\n";
+}
 int main()
 {
-    int num;
-    int guess;
-    int tries;
-
-    srand(time(NULL));
-    num = rand() % 100 + 1;
-
-    do
-    {
-        std::cout << "Enter a guess between 1 and 100: ";
-        std::cin >> guess;
-        tries++;
-
-        if (guess < num)
-        {
-            std::cout << "Guess is too low\n";
-        }
-        else if (guess > num)
-        {
-            std::cout << "Guess is too high\n";
-        }
-        else
-        {
-            std::cout << "Correct! The number of attempts was " << tries;
-        }
-
-    } while (guess != num);
+    happyBirthday();
+    happyBirthday();
+    happyBirthday();
 }
